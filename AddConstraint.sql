a1336c691fa48d6b1faab512bdcaaa22507e37d6
hodayaf01 <55048113+hodayaf01@users.noreply.github.com>
2019-10-27 21:55:22 +0200

    Add files via upload

---
+++ AddConstraint.sql
@@ -0,0 +1,26 @@
+﻿alter table [dbo].[MedicinesToChild]
+	add CONSTRAINT fk_KingOfDosage FOREIGN KEY ([Dosage])
+	REFERENCES [dbo].[KingOfDosage]([kindOfDosageId])
+
+alter table [dbo].[MedicinesToChild]
+	add CONSTRAINT fk_medicineId FOREIGN KEY ([medicineId])
+	REFERENCES [dbo].[Medicines] ([medicineId])
+
+	alter table [dbo].[MedicinesToChild]
+	add CONSTRAINT fk_childId FOREIGN KEY ([childId])
+	REFERENCES [dbo].[Users] ([Id])
+
+	alter table [dbo].[Users]
+	add CONSTRAINT fk_userHMO FOREIGN KEY ([userHMO])
+	REFERENCES [dbo].[HMO] ([IdHMO])
+
+	alter table [dbo].[guardiansToUser]
+	add CONSTRAINT fk_userId FOREIGN KEY ([userId])
+	REFERENCES [dbo].[Users] ([Id])
+
+	alter table [dbo].[guardiansToUser]
+	add CONSTRAINT fk_guardianId FOREIGN KEY ([guardianId])
+	REFERENCES [dbo].[Guardian] ([Id])
+
+ALTER TABLE [dbo].[guardiansToUser]
+	DROP CONSTRAINT fk_
